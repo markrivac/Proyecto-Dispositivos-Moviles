e5a7f6b832c4042b448a289ecc0c4bd0c4484bf4
Mark <mark.rivac@gmail.com>
2017-04-03 13:03:35 -0500

    Ability to answer a question

---
+++ app/src/main/java/com/example/davidcabello/proyectomoviles/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.davidcabello.proyectomoviles;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firebaseAuth = FirebaseAuth.getInstance();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+        if (id == R.id.nav_login) {
+            Intent intent = new Intent(this, Login.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_register) {
+            Intent intent2 = new Intent(this, Registro.class);
+            startActivity(intent2);
+
+        } else if (id == R.id.nav_signout) {
+            firebaseAuth.signOut();
+            Toast.makeText(this, "Logged out", Toast.LENGTH_SHORT).show();
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_algoritmos) {
+            Intent intent = new Intent(this, Categoria.class);
+            intent.putExtra("CATEGORY", "algoritmos");
+            startActivity(intent);
+
+        } else if (id == R.id.nav_db) {
+            Intent intent = new Intent(this, Categoria.class);
+            intent.putExtra("CATEGORY", "bases");
+            startActivity(intent);
+
+        } else if (id == R.id.nav_net) {
+            Intent intent = new Intent(this, Categoria.class);
+            intent.putExtra("CATEGORY", "redes");
+            startActivity(intent);
+
+        } else if (id == R.id.nav_vg) {
+            Intent intent = new Intent(this, Categoria.class);
+            intent.putExtra("CATEGORY", "videojuegos");
+            startActivity(intent);
+
+        }
+
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
